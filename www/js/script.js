03324cf4c35eb4e1f5d10e7a2c745f984edbab3c
juliozarate5 <juliozarate5@yahoo.com>
2018-02-14 23:26:46 -0500

    Update script.js

--- www/js/script.js
+++ www/js/script.js
@@ -16,14 +16,14 @@ var app={
   
   iniciaBotones: function() {
     var iframeContent = document.querySelector('#app');
-    iframeContent.addEventListener('click' ,this.refrescarIframe ,true);
+    iframeContent.addEventListener('click' ,this.refrescarIframe ,false);
   },
   
   refrescarIframe: function() {
     var online = navigator.onLine;
     if (online == true) {
     var iframeContent = document.querySelector('#app');
-    iframeContent.innerHTML = "<iframe id='myIframe' src='https://mangoapp.co/' frameborder=0  scrolling='yes' width=100%></iframe>";
+    iframeContent.innerHTML = "<iframe id='myIframe' height='650px' src='https://mangoapp.co/' frameborder=0  scrolling='yes' width=100%></iframe>";
     } else {
     iframeContent.innerHTML ="NO HAY CONEXION A INTERNET :(";
     }
@@ -35,4 +35,5 @@ if ('addEventListener' in document) {
     app.inicio();
   }, false);
 };
+
 
