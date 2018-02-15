17174e27c120b5b860c922d928c742e61e89bf68
juliozarate5 <juliozarate5@yahoo.com>
2018-02-14 23:21:25 -0500

    Update script.js

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,38 @@
-  function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-  }
+var app={
+    inicio: function(){
+    this.iniciaFastClick();
+    this.iniciaBotones();
+    this.refrescarIframe();
+    this.resizeIframe();
+  },
+  
+  iniciaFastClick: function() {
+    FastClick.attach(document.body);
+  },
+  
+  resizeIframe: function(obj){
+      obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  },
+  
+  iniciaBotones: function() {
+    var iframeContent = document.querySelector('#app');
+    iframeContent.addEventListener('click' ,this.refrescarIframe ,true);
+  },
+  
+  refrescarIframe: function() {
+    var online = navigator.onLine;
+    if (online == true) {
+    var iframeContent = document.querySelector('#app');
+    iframeContent.innerHTML = "<iframe id='myIframe' src='https://mangoapp.co/' frameborder=0  scrolling='yes' width=100%></iframe>";
+    } else {
+    iframeContent.innerHTML ="NO HAY CONEXION A INTERNET :(";
+    }
+  },
+};
+
+if ('addEventListener' in document) {
+  document.addEventListener("deviceready", function() {
+    app.inicio();
+  }, false);
+};
+
